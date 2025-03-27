635b69036c6538f2c6deea9a358a88da82c64385
Chidiebere-Nwigwe <chidonnwigz@gmail.com>
2025-03-27 01:50:55 -0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Chidiebere Nwigwe</h1>
-      <h2>TECH2102:Enterprise Computing-25JANMNFS2</h2>
+      <h1>Name: Chidiebere Nwigwe</h1>
+      <h2>Course Name: TECH2102:Enterprise Computing-25JANMNFS2</h2>
     </div>
 
   );
