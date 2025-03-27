92488d9795645cdd6ce4264497d3216276f18eb3
Chidiebere-Nwigwe <chidonnwigz@gmail.com>
2025-03-27 01:41:51 -0600

    Testing if pipeline automatic works by updating text by adding course name to web page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
   return (
-      <h1>Chidiebere Nwigwe Godwin</h1>
+    <div>
+      <h1>Chidiebere Nwigwe</h1>
+      <h2>TECH2102:Enterprise Computing-25JANMNFS2</h2>
+    </div>
+
   );
 }
 
